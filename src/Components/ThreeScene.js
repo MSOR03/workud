eeda5efee0c9b7c2364309e75713eddb032dbabe
MSOR03 <molartera@unal.edu.co>
2023-12-13 00:18:07 -0500

    Was improved scene start

--- src/Components/ThreeScene.js
+++ src/Components/ThreeScene.js
@@ -1,76 +1,122 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import * as THREE from 'three';
-import earthTexture from '../Images/imgscene.jpg'; // Reemplaza con la ruta correcta
-import styles from "../Styles/SceneOne.module.css"
-
-// ...
+import earthTexture from '../Images/texture.jpg';
+import styles from "../Styles/SceneOne.module.css";
 
 const ThreeScene = () => {
+  const sceneRef = useRef(null);
+
   useEffect(() => {
-    // Configuración de la escena
+    let mouseX = 0;
+    let mouseY = 0;
+    let zoom = 5;
+    let isMouseDown = false;
+
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-    const renderer = new THREE.WebGLRenderer({ alpha: true }); // Configura el fondo como transparente
+    const renderer = new THREE.WebGLRenderer({ alpha: true });
     renderer.setSize(window.innerWidth, window.innerHeight);
-    document.getElementById('scene-container').appendChild(renderer.domElement);
+    const container = document.getElementById('scene-container');
+    container.appendChild(renderer.domElement);
 
-    // Crear una esfera con textura
     const geometry = new THREE.SphereGeometry(2, 32, 32);
     const texture = new THREE.TextureLoader().load(earthTexture);
     const material = new THREE.MeshBasicMaterial({ map: texture, transparent: true });
     const earth = new THREE.Mesh(geometry, material);
     scene.add(earth);
 
-    // Configuración de la cámara
-    camera.position.z = 5;
+    camera.position.z = zoom;
 
-    // Animación
     const animate = () => {
       requestAnimationFrame(animate);
 
-      // Ajusta el tamaño del renderizador al cambiar el tamaño de la ventana
       camera.aspect = window.innerWidth / window.innerHeight;
       camera.updateProjectionMatrix();
       renderer.setSize(window.innerWidth, window.innerHeight);
 
-      // Rotar la esfera (Tierra)
-      earth.rotation.y += 0.01;
+      earth.rotation.y += 0.001;
 
-      // Renderizar la escena
+      // Actualizar la posición de la cámara basada en el movimiento del ratón
+      const targetX = mouseX * 0.005;
+      const targetY = mouseY * 0.005;
+
+      camera.position.x += (targetX - camera.position.x) * 0.1;
+      camera.position.y += (-targetY - camera.position.y) * 0.1;
+
+      camera.lookAt(scene.position);
+
       renderer.render(scene, camera);
     };
 
     animate();
+
     const handleResize = () => {
       camera.aspect = window.innerWidth / window.innerHeight;
       camera.updateProjectionMatrix();
       renderer.setSize(window.innerWidth, window.innerHeight);
     };
-  
-    window.addEventListener('resize', handleResize);
 
-    // Limpiar la escena al desmontar el componente
+    const handleMouseMove = (event) => {
+      mouseX = (event.clientX / window.innerWidth) * 2 - 1;
+      mouseY = - (event.clientY / window.innerHeight) * 2 + 1;
+    };
+
+    const handleWheel = (event) => {
+      // Ajustar la posición de la cámara basada en la rueda del mouse
+      zoom += event.deltaY * 0.005;
+      camera.position.z = zoom;
+    };
+
+    const handleMouseDown = () => {
+      isMouseDown = true;
+    };
+
+    const handleMouseUp = () => {
+      isMouseDown = false;
+    };
+
+    const handleMouseMoveDrag = (event) => {
+      if (isMouseDown) {
+        const deltaX = event.movementX || event.mozMovementX || event.webkitMovementX || 0;
+        const deltaY = event.movementY || event.mozMovementY || event.webkitMovementY || 0;
+
+        const rotationSpeed = 0.002;
+
+        earth.rotation.y += deltaX * rotationSpeed;
+        earth.rotation.x += deltaY * rotationSpeed;
+
+        // Limitar la rotación en el eje X para evitar que la escena se invierta
+        earth.rotation.x = Math.max(-Math.PI / 2, Math.min(Math.PI / 2, earth.rotation.x));
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+    window.addEventListener('mousemove', handleMouseMove);
+    window.addEventListener('wheel', handleWheel);
+    window.addEventListener('mousedown', handleMouseDown);
+    window.addEventListener('mouseup', handleMouseUp);
+    window.addEventListener('mousemove', handleMouseMoveDrag);
+
     return () => {
       window.removeEventListener('resize', handleResize);
-      const sceneContainer = document.getElementById('scene-container');
-      if (sceneContainer) {
-        sceneContainer.removeChild(renderer.domElement);
-      }
+      window.removeEventListener('mousemove', handleMouseMove);
+      window.removeEventListener('wheel', handleWheel);
+      window.removeEventListener('mousedown', handleMouseDown);
+      window.removeEventListener('mouseup', handleMouseUp);
+      window.removeEventListener('mousemove', handleMouseMoveDrag);
+      container.removeChild(renderer.domElement);
     };
   }, []);
 
-  
-
   return (
     <div className={styles.container}>
-      <div className={styles.sceneContainer} id="scene-container" />
+      <div className={styles.sceneContainer} id="scene-container" ref={sceneRef} />
       <div className={styles.textContainer}>
-
+        {/* Puedes agregar contenido adicional en el contenedor de texto si es necesario */}
       </div>
     </div>
   );
 };
 
 export { ThreeScene };
-
